bf4aa31c7e8b0c36d37fd9fa0ba3ca6988cd186a
hewei <heweimailbox@foxmail.com>
2019-09-17 22:23:00 +0800

    regular update

--- nodePractice/practiceCLI.js
+++ nodePractice/practiceCLI.js
@@ -6,9 +6,28 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-rl.question('What do you think of Node.js? ', (answer) => {
-    // TODO: Log the answer in a database
-    console.log(`Thank you for your valuable feedback: ${answer}`);
+// rl.question('What do you think of Node.js? ', (answer) => {
+//     // TODO: Log the answer in a database
+//     console.log(`Thank you for your valuable feedback: ${answer}`);
 
+//     rl.close();
+// });
+
+/**
+ * @description readline 学习  
+ * 1. line事件  遇到 \n(newline 向下移动一行，并不移动左右) ,\r(return 到当前行的最左边) 或 \n\r。
+ * Linux中\n表示回车+换行；Windows中\r\n表示回车+换行。
+ */
+
+rl.on('line', function(input) {
+    console.log(`Event line, you input: ${input}`);
     rl.close();
-});
+})
+
+/**
+ * @des  close 事件
+ */
+rl.on('close', function(input) {
+    console.log(`Event line, you input: ${input}`);
+    rl.close();
+})
