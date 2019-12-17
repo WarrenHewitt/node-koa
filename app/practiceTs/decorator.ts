5c189f0c22f56da74d45263a63ccb9e271f8bf48
NameHewei <heweimailbox@foxmail.com>
2019-12-17 13:08:18 +0800

    regular update

--- app/practiceTs/decorator.ts
+++ app/practiceTs/decorator.ts
@@ -13,16 +13,54 @@ function manage() {
 }
 
 
+/** 
+ * 类装饰器: 类装饰器应用于类构造函数，可以用来监视，修改或替换类定义 
+ * 1. 利用继承添加属性
+ */
+function classExtend (target: any) {
+    return class extends target {
+        age = 1
+        constructor(age: number, name: String){
+            super(name)
+            this.age = age
+        }
+    }
+}
+
+function classLog(originConstructor: Function) {
+    const original = originConstructor;
+
+
+    const fn = function() {
+
+    }
+
+    fn.prototype = original.prototype
+
+    return fn
+}
+
+
+// @classLog
+@classExtend
 class Dec {
+    name:String = ''
+
+    constructor(name: String) {
+        this.name = name
+    }
+
     /**
-     * @des 组合：由上往下，
+     * @des 组合：由上往下求值，由下往上依次调用
      */
     @fac()
     @manage()
-    peoduct() {}
+    product() {
+        console.log('product: ', this.name);
+    }
 }
 
 export default function () {
-    const dec = new Dec()
+    const dec = new Dec('hew')
     console.log('=========');
 } 
--- server/practiceTs/decorator.js
+++ server/practiceTs/decorator.js
@@ -18,18 +18,48 @@ function manage() {
         console.log('manage deco');
     };
 }
-class Dec {
-    /**
-     * @des 组合：由上往下，
-     */
-    peoduct() { }
+/**
+ * 类装饰器: 类装饰器应用于类构造函数，可以用来监视，修改或替换类定义
+ * 1. 利用继承添加属性
+ */
+function classExtend(target) {
+    return class extends target {
+        constructor(age, name) {
+            super(name);
+            this.age = 1;
+            this.age = age;
+        }
+    };
 }
+function classLog(originConstructor) {
+    const original = originConstructor;
+    const fn = function () {
+    };
+    fn.prototype = original.prototype;
+    return fn;
+}
+// @classLog
+let Dec = class Dec {
+    constructor(name) {
+        this.name = '';
+        this.name = name;
+    }
+    /**
+     * @des 组合：由上往下求值，由下往上依次调用
+     */
+    product() {
+        console.log('product: ', this.name);
+    }
+};
 __decorate([
     fac(),
     manage()
-], Dec.prototype, "peoduct", null);
+], Dec.prototype, "product", null);
+Dec = __decorate([
+    classExtend
+], Dec);
 function default_1() {
-    const dec = new Dec();
+    const dec = new Dec('hew');
     console.log('=========');
 }
 exports.default = default_1;
